330bbe3356322fd8a84a34994954bef141c14c54
Jeroen Van Steirteghem <jeroen.vansteirteghem@gmail.com>
2013-05-29 14:36:07 +0200

    tests

--- tests/test_twunnel.py
+++ tests/test_twunnel.py
@@ -5,6 +5,9 @@ from test import twunnel
 from twisted.internet import protocol
 from twisted.trial import unittest
 from twisted.test import proto_helpers
+import base64
+import struct
+import socket
 
 class TestTunnelProtocol(protocol.Protocol):
     def __init__(self):
@@ -35,32 +38,27 @@ class HTTPTunnelTestCase(unittest.TestCase):
                 }
             }
         }
-        self.address = "127.0.0.1"
-        self.port = 80
+        self.remoteAddress = "127.0.0.1"
+        self.remotePort = 80
         
-        self.protocolFactory = twunnel.HTTPTunnelOutputProtocolFactory(self.configuration, self.address, self.port, TestTunnelProtocol())
+        self.protocolFactory = twunnel.HTTPTunnelOutputProtocolFactory(self.configuration, self.remoteAddress, self.remotePort, TestTunnelProtocol())
         self.protocolFactory.protocol = twunnel.HTTPTunnelOutputProtocol
-        self.protocol = self.protocolFactory.buildProtocol((self.address, self.port))
+        self.protocol = self.protocolFactory.buildProtocol((self.remoteAddress, self.remotePort))
         self.transport = proto_helpers.StringTransport()
         self.protocol.makeConnection(self.transport)
-
-    def test_response_200(self):
-        self.assertEqual(self.transport.value(), "CONNECT %s:%d HTTP/1.0\r\n\r\n" % (self.address, self.port))
         
+    def tearDown(self):
+        self.transport.loseConnection()
+        
+    def test(self):
+        value = self.transport.value()
         self.transport.clear()
+        
+        self.assertEqual(value, "CONNECT %s:%d HTTP/1.0\r\n\r\n" % (self.remoteAddress, self.remotePort))
+        
         self.protocol.dataReceived("HTTP/1.0 200 OK\r\n\r\n")
-        
-        self.assertEqual(self.transport.disconnecting, False)
-    
-    def test_response_401(self):
-        self.assertEqual(self.transport.value(), "CONNECT %s:%d HTTP/1.0\r\n\r\n" % (self.address, self.port))
-        
-        self.transport.clear()
-        self.protocol.dataReceived("HTTP/1.0 401 Unauthorized\r\n\r\n")
-        
-        self.assertEqual(self.transport.disconnecting, True)
 
-class HTTPTunnelWithBasicAuthenticationTestCase(unittest.TestCase):
+class HTTPTunnelBasicAuthenticationTestCase(unittest.TestCase):
     def setUp(self):
         self.configuration = {
             "PROXY_SERVER": {
@@ -73,30 +71,143 @@ class HTTPTunnelWithBasicAuthenticationTestCase(unittest.TestCase):
                 }
             }
         }
-        self.address = "127.0.0.1"
-        self.port = 80
+        self.remoteAddress = "127.0.0.1"
+        self.remotePort = 80
         
-        self.protocolFactory = twunnel.HTTPTunnelOutputProtocolFactory(self.configuration, self.address, self.port, TestTunnelProtocol())
+        self.protocolFactory = twunnel.HTTPTunnelOutputProtocolFactory(self.configuration, self.remoteAddress, self.remotePort, TestTunnelProtocol())
         self.protocolFactory.protocol = twunnel.HTTPTunnelOutputProtocol
-        self.protocol = self.protocolFactory.buildProtocol((self.address, self.port))
+        self.protocol = self.protocolFactory.buildProtocol((self.remoteAddress, self.remotePort))
         self.transport = proto_helpers.StringTransport()
         self.protocol.makeConnection(self.transport)
-
-    def test_response_200(self):
-        self.assertEqual(self.transport.value(), "CONNECT %s:%d HTTP/1.0\r\nProxy-Authorization: Basic MToy\r\n\r\n" % (self.address, self.port))
         
+    def tearDown(self):
+        self.transport.loseConnection()
+        
+    def test(self):
+        value = self.transport.value()
         self.transport.clear()
+        
+        self.assertEqual(value, "CONNECT %s:%d HTTP/1.0\r\nProxy-Authorization: Basic %s\r\n\r\n" % (self.remoteAddress, self.remotePort, base64.standard_b64encode("%s:%s" % (self.configuration["PROXY_SERVER"]["AUTHENTICATION"]["USERNAME"], self.configuration["PROXY_SERVER"]["AUTHENTICATION"]["PASSWORD"]))))
+        
         self.protocol.dataReceived("HTTP/1.0 200 OK\r\n\r\n")
+
+class SOCKS5TunnelIPv4TestCase(unittest.TestCase):
+    def setUp(self):
+        self.configuration = {
+            "PROXY_SERVER": {
+                "TYPE": "SOCKS5",
+                "ADDRESS": "127.0.0.1",
+                "PORT": 1080,
+                "AUTHENTICATION": {
+                    "USERNAME": "",
+                    "PASSWORD": ""
+                }
+            }
+        }
+        self.remoteAddress = "127.0.0.1"
+        self.remotePort = 80
         
-        self.assertEqual(self.transport.disconnecting, False)
-    
-    def test_response_401(self):
-        self.assertEqual(self.transport.value(), "CONNECT %s:%d HTTP/1.0\r\nProxy-Authorization: Basic MToy\r\n\r\n" % (self.address, self.port))
+        self.protocolFactory = twunnel.SOCKS5TunnelOutputProtocolFactory(self.configuration, self.remoteAddress, self.remotePort, TestTunnelProtocol())
+        self.protocolFactory.protocol = twunnel.SOCKS5TunnelOutputProtocol
+        self.protocol = self.protocolFactory.buildProtocol((self.remoteAddress, self.remotePort))
+        self.transport = proto_helpers.StringTransport()
+        self.protocol.makeConnection(self.transport)
         
+    def tearDown(self):
+        self.transport.loseConnection()
+        
+    def test(self):
+        value = self.transport.value()
         self.transport.clear()
-        self.protocol.dataReceived("HTTP/1.0 401 Unauthorized\r\n\r\n")
         
-        self.assertEqual(self.transport.disconnecting, True)
+        v, c, r = struct.unpack('!BBB', value[:3])
+        
+        self.assertEqual(v, 0x05)
+        self.assertEqual(c, 0x01)
+        self.assertEqual(r, 0x00)
+        
+        value = struct.pack('!BB', 0x05, 0x00)
+        
+        self.protocol.dataReceived(value)
+        
+        value = self.transport.value()
+        self.transport.clear()
+        
+        v, c, r, remoteAddressType = struct.unpack('!BBBB', value[:4])
+        
+        self.assertEqual(v, 0x05)
+        self.assertEqual(c, 0x01)
+        self.assertEqual(r, 0x00)
+        self.assertEqual(remoteAddressType, 0x01)
+        
+        remoteAddress, remotePort = struct.unpack('!IH', value[4:10])
+        remoteAddress = socket.inet_ntoa(struct.pack('!I', remoteAddress))
+        
+        self.assertEqual(remoteAddress, self.remoteAddress)
+        self.assertEqual(remotePort, self.remotePort)
+        
+        value = struct.pack('!BBBBIH', 0x05, 0x00, 0x00, 0x01, 0, 0)
+        
+        self.protocol.dataReceived(value)
+
+class SOCKS5TunnelDNTestCase(unittest.TestCase):
+    def setUp(self):
+        self.configuration = {
+            "PROXY_SERVER": {
+                "TYPE": "SOCKS5",
+                "ADDRESS": "127.0.0.1",
+                "PORT": 1080,
+                "AUTHENTICATION": {
+                    "USERNAME": "",
+                    "PASSWORD": ""
+                }
+            }
+        }
+        self.remoteAddress = "localhost"
+        self.remotePort = 80
+        
+        self.protocolFactory = twunnel.SOCKS5TunnelOutputProtocolFactory(self.configuration, self.remoteAddress, self.remotePort, TestTunnelProtocol())
+        self.protocolFactory.protocol = twunnel.SOCKS5TunnelOutputProtocol
+        self.protocol = self.protocolFactory.buildProtocol((self.remoteAddress, self.remotePort))
+        self.transport = proto_helpers.StringTransport()
+        self.protocol.makeConnection(self.transport)
+        
+    def tearDown(self):
+        self.transport.loseConnection()
+        
+    def test(self):
+        value = self.transport.value()
+        self.transport.clear()
+        
+        v, c, r = struct.unpack('!BBB', value[:3])
+        
+        self.assertEqual(v, 0x05)
+        self.assertEqual(c, 0x01)
+        self.assertEqual(r, 0x00)
+        
+        value = struct.pack('!BB', 0x05, 0x00)
+        
+        self.protocol.dataReceived(value)
+        
+        value = self.transport.value()
+        self.transport.clear()
+        
+        v, c, r, remoteAddressType = struct.unpack('!BBBB', value[:4])
+        
+        self.assertEqual(v, 0x05)
+        self.assertEqual(c, 0x01)
+        self.assertEqual(r, 0x00)
+        self.assertEqual(remoteAddressType, 0x03)
+        
+        remoteAddressLength = ord(value[4])
+        remoteAddress, remotePort = struct.unpack('!%dsH' % remoteAddressLength, value[5:])
+        
+        self.assertEqual(remoteAddress, self.remoteAddress)
+        self.assertEqual(remotePort, self.remotePort)
+        
+        value = struct.pack('!BBBBIH', 0x05, 0x00, 0x00, 0x01, 0, 0)
+        
+        self.protocol.dataReceived(value)
 
 if __name__ == "__main__":
     unittest.main()
